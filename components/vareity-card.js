56e5e8691f0fb8d349640dd7f6b14e9f2abae7f4
Mateusz <sliwowski.mateusz@wp.pl>
2023-05-17 14:49:04 +0200

    make stock prop work, card UI changes

--- components/vareity-card.js
+++ components/vareity-card.js
@@ -7,9 +7,12 @@ const vareityCard = {
                           class="white--text align-end"
                           cover
                         >
-                          <v-card-title class="text-h6 py-2">
+                          <v-card-title class="pb-0">
                             {{ item.name }}
                           </v-card-title>
+                          <v-card-subtitle class="pb-2">
+                            {{ item.type }}
+                          </v-card-subtitle>
                         </v-img>
       
                         <v-card-actions>
@@ -19,7 +22,9 @@ const vareityCard = {
                               }}</v-icon
                             >
                           </v-btn>
-                          <v-btn text> Listen Now </v-btn>
+                          <v-icon v-if="stock.length">{{!availableStock.length ? 'mdi-signal-cellular-outline' : 'mdi-signal-cellular-3'}}</v-icon>
+                          <v-spacer></v-spacer>
+                          <v-btn text> więcej </v-btn>
                         </v-card-actions>
       
                         <v-expand-transition>
@@ -39,7 +44,7 @@ const vareityCard = {
                             </v-list>
                             
 
-                            <v-list :bg-color="item.color" v-if="$attrs.stock.length" lines="one">
+                            <v-list :bg-color="item.color" v-if="stock.length" lines="one">
                             <v-list-subheader>Dostępność</v-list-subheader>
                               <v-list-item
                                 v-if="availableStock.length"
@@ -59,16 +64,21 @@ const vareityCard = {
       type: Object,
       required: true,
     },
+    stock: {
+      type: Array,
+      required: false,
+      default: []
+    },
   },
   data() {
     return {
       prices: [],
-      show: true,
+      show: false,
     };
   },
   computed: {
     availableStock() {
-      return this.$attrs.stock.filter(
+      return this.stock.filter(
         (element) =>
           this.item.name.toUpperCase() == element["Product_name"]
       );
