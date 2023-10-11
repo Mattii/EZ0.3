407bfa89d10c16d80a638e353f1c9c8fde785c25
Mateusz <sliwowski.mateusz@wp.pl>
2023-10-11 14:16:42 +0200

    refactoring stock imput fun to use multiple file

--- components/stock-input.js
+++ components/stock-input.js
@@ -2,6 +2,7 @@ import { ref, computed, onMounted, watch } from "vue";
 const stocImput = {
   template: `
     <v-file-input
+    multiple
     prepend-icon=""
     variant="solo"
     label="Wyszukaj raport"
@@ -26,44 +27,59 @@ const stocImput = {
   emits:['stockReady'],
   setup(props, context) {
 
-    function readJsonFile(rawFile) {
+    async function readJsonFile(rawFile) {
+
+      for(let e in rawFile.target.files){
+        console.log(rawFile.target.files[e]);
+      }
+
+     const test = await convertXLSXtoJSON(rawFile.target.files[0])
+     stockRaportEmiter(test);
+    }
+
+    const convertXLSXtoJSON = (rawFile) => {
+      
       let fileReader = new FileReader();
 
-      fileReader.onload = (event) => {
-        let data = event.target.result;
-        console.log(event.target.result);
-        if (window.Worker) {
-          // …
-          const myWorker = new Worker("/workers/xlsx.js");
-          console.log("worker works");
-          console.log("Message posted to worker");
-          myWorker.postMessage(data);
-          myWorker.onmessage = (e) => {
-            stockRaportEmiter(e.data)
-            console.log("Message received from worker");
-          };
-        } else {
-          let workbook = XLSX.read(data, { type: "binary", cellDates: true });
-          workbook.SheetNames.forEach((sheet) => {
-            let rawStock = XLSX.utils.sheet_to_row_object_array(
-              workbook.Sheets[sheet],
-              { dateNF: "yyyy-mm" }
-            );
+      fileReader.readAsBinaryString(rawFile);
 
-            stockRaportEmiter(rawStock.map((ele) => {
-              return Object.assign(
-                {},
-                ...Object.keys(ele).map((key) => {
-                  let newKey = key.replace(/\n/g, " ").replace(/\s+/g, "_");
-                  return { [newKey]: ele[key] };
-                })
+      return new Promise((resolve, reject) => {
+        fileReader.onload = (event) => {
+          let data = event.target.result;
+          if (window.Worker) {
+            // …
+            const myWorker = new Worker("/workers/xlsx.js");
+            console.log("worker works");
+            console.log("Message posted to worker");
+            myWorker.postMessage(data);
+            myWorker.onmessage = (e) => {
+              console.log("Message received from worker");
+              resolve(e.data)
+            };
+          } else {
+            let workbook = XLSX.read(data, { type: "binary", cellDates: true });
+  
+            workbook.SheetNames.forEach((sheet) => {
+              let rawStock = XLSX.utils.sheet_to_row_object_array(
+                workbook.Sheets[sheet],
+                { dateNF: "yyyy-mm" }
               );
-            }))
-          });
-        }
-        
-      };
-      fileReader.readAsBinaryString(rawFile.target.files[0]);
+  
+              let stock = rawStock.map((ele) => {
+                return Object.assign(
+                  {},
+                  ...Object.keys(ele).map((key) => {
+                    let newKey = key.replace(/\n/g, " ").replace(/\s+/g, "_");
+                    return { [newKey]: ele[key] };
+                  })
+                );
+              });
+              resolve(stock)
+            });
+          }
+        };
+      })
+      
     }
 
     function stockRaportEmiter(stock) {
