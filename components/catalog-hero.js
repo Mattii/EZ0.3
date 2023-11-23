95416d0ffeeec8c7ffea56ef0bc61cbc652f92a2
Mateusz <sliwowski.mateusz@wp.pl>
2023-11-23 14:20:06 +0100

    catalog hero res img added

--- components/catalog-hero.js
+++ components/catalog-hero.js
@@ -12,14 +12,14 @@ const heroElement = {
           <v-img
           cover
             width="100%"
-            src="https://res.cloudinary.com/ddkef5waq/image/upload/v1700734253/enzapp/pricehero14000x400_kh4zst.png"
+            src="https://res.cloudinary.com/ddkef5waq/image/upload/v1700745362/kataloghero1400x400_s12wgg.png"
           >
             <template #sources>
-              <source media="(max-width: 460px)" srcset="https://res.cloudinary.com/ddkef5waq/image/upload/v1700733238/enzapp/pricehero300x200_kqjas8.png">
-              <source media="(max-width: 690px)" srcset="https://res.cloudinary.com/ddkef5waq/image/upload/v1700733135/enzapp/pricehero600x200_gunlex.png">
-              <source media="(max-width: 800px)" srcset="https://res.cloudinary.com/ddkef5waq/image/upload/v1700733815/enzapp/pricehero800x300_c2ebjz.png">
+              <source media="(max-width: 460px)" srcset="https://res.cloudinary.com/ddkef5waq/image/upload/v1700745362/kataloghero300x200_wam47j.png">
+              <source media="(max-width: 690px)" srcset="https://res.cloudinary.com/ddkef5waq/image/upload/v1700745362/kataloghero600x200_gnd8kj.png">
+              <source media="(max-width: 800px)" srcset="https://res.cloudinary.com/ddkef5waq/image/upload/v1700745362/kataloghero800x300_ctamac.png">
             </template>
-          <div class="px-9 pb-9 px-sm-12 pb-sm-12 h-100 ff-nunito d-flex align-end">
+          <div class="px-9 pb-9 px-sm-12 pb-sm-12 pb-lg-14 px-lg-14 h-100 ff-nunito d-flex align-end">
             <h2 class="text-h4 text-md-h3 text-lg-h2 ff-nunito">Katalog</h2>
           </div>
           </v-img>
