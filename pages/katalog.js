95ae5adc28d7648ed647ef9e0ac60d5a2f84c12c
Mateusz <sliwowski.mateusz@wp.pl>
2024-08-30 13:17:05 +0200

    added serch and filter components to katalog

--- pages/katalog.js
+++ pages/katalog.js
@@ -1,9 +1,10 @@
-import { ref, reactive, onMounted } from "vue";
+import { ref, reactive,computed, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import { useStore } from 'vuex';
 import { getDatabase, query, orderByValue, orderByKey, orderByChild, onValue, startAt, endAt } from  "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
 import {ref as fref } from  "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
 import app from "../modules/firebase.js"
+import cropCodeToFullCropName from "../modules/cropCodeToFullCropName.js"
 
 const crop = {
   template: `          
@@ -13,6 +14,71 @@ const crop = {
         <catalog-hero></catalog-hero>
       </v-col>
     </v-row>
+
+    <v-row justify="center">
+      <v-col  xs="12" sm="8" md="6" lg="4" class="">
+        <v-text-field
+          clearable 
+          prepend-icon=""
+          variant="solo"
+          label="Wyszukaj odmianę"
+          truncate-length="15"
+          density="compact"
+          bg-color="primary"
+          rounded="pill"
+          v-model="searchValue"
+          @click:clear="() => {searchValue = ''}"
+        > 
+          <template v-slot:append>
+            <v-btn
+              color="secondary"
+              icon="mdi-magnify"
+            ></v-btn>
+            <v-btn
+            class="ml-3"
+            color="secondary"
+            icon="mdi-filter"
+            @click="() => {
+              filterShow = !filterShow;
+              filterValues = []
+              }"
+            >
+          </v-btn>
+          </template>
+        </v-text-field>
+        <v-slide-y-transition>
+          <v-select
+            v-if="filterShow"
+            v-model="filterValues"
+            :items="filterItems"
+            variant="solo"
+            density="compact"
+            bg-color="primary"
+            rounded="pill"
+            label="Wybierz segment"
+            clearable
+            multiple
+          >
+            <template v-slot:selection="{ item, index }">
+              <v-chip v-if="index < 3">
+                <span>{{ cropCodeToFullCropName(item.title) }}</span>
+              </v-chip>
+              <span
+                v-if="index === 3"
+                class="text-lightgrey text-caption align-self-center"
+              >
+                (+{{ filterValues.length - 3 }} )
+              </span>
+            </template>
+
+            <template v-slot:item="{ props, item }">
+              <v-list-item v-bind="props" :title="cropCodeToFullCropName(item.title)"></v-list-item>
+            </template>
+          </v-select>
+        </v-slide-y-transition>
+        </v-col>
+    </v-row>
+
     <v-row justify="center" class="">
     <v-col  xs="12" sm="10" md="9" lg="8" class="pa-0 ma-0 d-flex flex-wrap justify-center">
           <v-col  xs="12" sm="6" md="4" class="d-flex flex-wrap justify-space-evenly"             
@@ -36,6 +102,11 @@ const crop = {
     const store = useStore();
     const crops = ref({});
     const stock = ref([]);
+    const filterShow = ref(false);
+    const filterValues = ref([]);
+    const searchValue = ref("");
+
+    const filterItems = computed(() => [...new Set(store.getters.getKatalogFromStore.map(ele => ele.crop))]);
 
     onMounted(async () => {
       const db = getDatabase(app);
@@ -57,7 +128,12 @@ const crop = {
       route,
       crops,
       stock,
+      filterShow,
+      filterItems,
+      filterValues,
       onMounted,
+      searchValue,
+      cropCodeToFullCropName,
     };
   },
 };
